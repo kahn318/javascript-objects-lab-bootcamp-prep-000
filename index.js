a3c7ba8239f2911780d482909a33a89225cb4c54
Christian Ahn <kwanghoonahn@gmail.com>
2017-10-25 22:50:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var recipes = {};
 
 // returns a clone of 'object' by adding 'key' and 'value' (non-destructive)
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, recipes, {key: value});
+  return Object.assign({}, objectg, {key: value});
 }
